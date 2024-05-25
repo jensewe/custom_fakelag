4d986af8f0f62b700b1519ba618e48bad387410c
Forgetest <33988868+jensewe@users.noreply.github.com>
2024-05-25 18:07:08 +0800

    test

--- NET_LagPacket_Detour.cpp
+++ NET_LagPacket_Detour.cpp
@@ -3,15 +3,16 @@
 #include "PlayerLagManager.h"
 #include "LagSystem.h"
 
-IForward* g_fwdLagPacket = NULL;
+// IForward* g_fwdLagPacket = NULL;
 CDetour* DLagPacket = NULL;
+CDetour* DSendPacket = NULL;
 
 static const PlayerLagManager* s_LagManager;
 static LagSystem* s_LagSystem;
 
-float getLagPacketMs(const _netpacket_t & packet)
+float getLagPacketMs(const dumb_netadr_s & adr)
 {
-	return s_LagManager->GetPlayerLag(packet.from);
+	return s_LagManager->GetPlayerLag(adr);
 }
 
 DETOUR_DECL_STATIC2(NET_LagPacket, bool, bool, newdata, _netpacket_t*, packet)
@@ -22,7 +23,7 @@ DETOUR_DECL_STATIC2(NET_LagPacket, bool, bool, newdata, _netpacket_t*, packet)
 	// Returning true means we assert that "packet" is full of a consumable network packet.
 	// Returning false means the consumer should ignore packet, there is no packet to consume yet.
 	if (newdata) {
-		const float lagTime = getLagPacketMs(*packet);
+		const float lagTime = getLagPacketMs(&packet->from);
 		if (lagTime > 0.0) {
 			// g_pSM->LogError(myself, "Lagging packet on socket %d for %fms", packet->source, lagTime);
 			s_LagSystem->LagPacket(packet, lagTime);
@@ -37,6 +38,17 @@ DETOUR_DECL_STATIC2(NET_LagPacket, bool, bool, newdata, _netpacket_t*, packet)
 	return s_LagSystem->GetNextPacket(packet->source, packet);
 }
 
+DETOUR_DECL_STATIC8(NET_SendPacket, int, INetChannel *, chan, int, sock,  const dumb_netadr_s &, to, const unsigned char *, data, int, length, bf_write *, pVoicePayload /* = NULL */, bool, bUseCompression /*=false*/, uint32, unMillisecondsDelay /*=0u*/)
+{
+	const float lagTime = getLagPacketMs(to);
+	if (lagTime > 0.0) {
+		// g_pSM->LogError(myself, "Lagging packet on socket %d for %fms", sock, lagTime);
+		unMillisecondsDelay += (uint32)lagTime;
+		return NET_SendPacket_Actual(chan, sock, to, data, length, pVoicePayload, bUseCompression, unMillisecondsDelay);
+	}
+	return NET_SendPacket_Actual(chan, sock, to, data, length, pVoicePayload, bUseCompression, unMillisecondsDelay);
+}
+
 bool CreateNetLagPacketDetour()
 {
 	DLagPacket = DETOUR_CREATE_STATIC(NET_LagPacket, "NET_LagPacket");
@@ -46,6 +58,15 @@ bool CreateNetLagPacketDetour()
 		return false;
 	}
 	DLagPacket->EnableDetour();
+
+	DSendPacket = DETOUR_CREATE_STATIC(NET_SendPacket, "NET_SendPacket");
+	if (DSendPacket == NULL)
+	{
+		g_pSM->LogError(myself, "NET_SendPacket detour could not be initialized - FeelsBadMan.");
+		return false;
+	}
+	DSendPacket->EnableDetour();
+
 	return true;
 }
 
